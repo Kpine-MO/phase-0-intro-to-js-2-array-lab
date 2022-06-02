1efa03c7d010db19f49c5bb43520b7345b6d9cec
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-02 03:30:25 +0300

    commit tree a3bf6f8c4e2aaaa2c2065e39e242e487ae319da4

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+var cats = [];
+
+cats.push('Milo','Otis','Garfield');
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+}
+
+function appendCat(name){
+
+   var newCats = [
+       ...cats,'Broom'
+   ]
+   return newCats
+  
+}
+
+function prependCat(name){
+    
+    var otherCats = [
+        'Arnold',...cats
+    ]
+    return otherCats
+   
+ }
+function removeLastCat(name) {
+    const slicedCats = cats.slice(0, -1)
+    return slicedCats
+}
+
+function removeFirstCat(name) {
+    const slicedCats = cats.slice(1)
+    return slicedCats
+}
